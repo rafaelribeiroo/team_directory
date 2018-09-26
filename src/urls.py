99fb494a638c5cb5138630e04b6a23be71a33cec
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-09-26 02:36:37 -0300

    Trabalhando no front

--- src/urls.py
+++ src/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from src.apps.www import urls as www_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include((www_urls), namespace='www')),
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
